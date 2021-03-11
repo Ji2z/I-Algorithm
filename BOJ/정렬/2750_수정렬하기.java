7a1322e9f77b489194e501a7ee094cd21f9dd72d
Ji2z <leegin98@naver.com>
2021-03-11 22:22:35 +0900

    (210311) 2751_수정렬하기2

---
+++ BOJ/정렬/2750_수정렬하기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @since 2021. 3. 11.
+ * @author Jin
+ * @see https://www.acmicpc.net/problem/2750
+ * @mem 12052kb
+ * @time 96ms
+ * @caution B1
+ */
+
+public class 2750_수정렬하기 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder output = new StringBuilder("");
+		
+		int N = Integer.parseInt(br.readLine());
+		List<Integer> list = new ArrayList<>();
+		
+		for (int i = 0; i < N; i++) {
+			list.add(Integer.parseInt(br.readLine()));
+		}
+		
+		Collections.sort(list);
+
+		for (int i = 0; i < list.size(); i++) {
+			output.append(list.get(i)).append("\n");
+		}
+		
+		System.out.println(output);
+	}
+}
